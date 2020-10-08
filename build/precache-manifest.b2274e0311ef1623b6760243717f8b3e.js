8d98e4beb8823a2c5641967ad1a4f8ec66b3e9c8
DevarshiKSagar <devarshi.sagar@gmail.com>
2020-10-07 20:08:51 -0400

    created single page application

---
+++ build/precache-manifest.b2274e0311ef1623b6760243717f8b3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8793cea03f3ba4636923",
+    "url": "/static/css/main.5517459b.chunk.css"
+  },
+  {
+    "revision": "8793cea03f3ba4636923",
+    "url": "/static/js/main.8793cea0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "b61ca02b184f0448d23b3e9bc8a5b43c",
+    "url": "/index.html"
+  }
+];
